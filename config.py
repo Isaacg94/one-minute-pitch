139aea126b86158ba3cebf544306a5656a625a81
Isaacg94 <7248zack@gmail.com>
2019-10-25 09:13:53 +0300

    heroku push

--- config.py
+++ config.py
@@ -21,10 +21,12 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 
 class DevConfig(Config):
+    
     '''
     Development  configuration child class
 
